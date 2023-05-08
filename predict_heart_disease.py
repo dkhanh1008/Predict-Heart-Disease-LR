6e73609e36e25cee314975fe0f0a3fc0b1c582b1
dkhanh1008 <96035490+dkhanh1008@users.noreply.github.com>
2023-05-07 23:24:41 -0500

    Update predict_heart_disease.py

--- predict_heart_disease.py
+++ predict_heart_disease.py
@@ -175,7 +175,7 @@ def main():
         X_test_continuous_scaled = np.array(X_test_continuous).reshape(1,-1)
         
         #scale all
-        df = pd.read_csv('Predict-Heart-Disease-LR/data/framingham.csv')
+        df = pd.read_csv('data/framingham.csv')
         scaler = scaler.fit(df[['age', 'cigsPerDay', 'totChol', 'sysBP', 'diaBP', 'BMI', 'heartRate']])
         
         scaled_value = scaler.transform(df_test)
